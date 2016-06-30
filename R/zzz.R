f4733665739c0470dc23ef12e86f1c4959b4f076
Dave Braze <davebraze@gmail.com>
2016-06-30 10:05:24 -0400

    zzz.R add .onAttach() for this package

---
+++ R/zzz.R
@@ -0,0 +1,21 @@
+# Use .onLoad for side effects of package.
+.onLoad <- function(lib,pkg) {
+    ## See HW page for thoughts on what to do here: http://r-pkgs.had.co.nz/r.html
+}
+
+# Use .onAttach for informative messages.
+.onAttach <- function(lib, pkg) {
+
+    packageStartupMessage("Welcome to ", pkg, ".")
+
+    ## Better to build the startupMessage by reading the Description file, as in:
+    ## ver <- read.dcf(file.path(lib, pkg, "DESCRIPTION"), "Version")
+    ## ver <- as.character(ver)
+    ## packageStartupMessage(paste("Welcome to",  pkg, ", version ", ver, "."))
+
+    packageStartupMessage("    (c) 2016, Dave Braze and others.")
+    packageStartupMessage("    Released under the MIT license.\n")
+
+    invisible()
+}
+
