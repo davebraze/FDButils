1eca5a959cf3675ccc869e5a33cc40c3cad7502a
Dave Braze <davebraze@gmail.com>
2021-10-21 22:31:56 -0400

    tuning package roxygen file

--- R/FDButils-package.R
+++ R/FDButils-package.R
@@ -1,11 +1,12 @@
 ##' @title Utility and Convenience Functions.
 ##'
 ##' @description This package is a repository for low-level
-##'     utility functions used in the FDB lab. As a rule,
+##'     utility functions used by FDB. As a rule,
 ##'     no function in this package will
 ##'     depend on anything beyond the R 'base' and 'recommended'
 ##'     packages.
 ##'
 ##' @name FDButils
 ##' @docType package
-NULL
+##' @keywords internal
+"_PACKAGE"
