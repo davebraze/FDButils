53dd6c6288502e483e01c58f27f5304bfd8d0feb
Dave Braze <davebraze@gmail.com>
2021-10-25 15:24:58 -0400

    doc tuning

--- R/openwd.R
+++ R/openwd.R
@@ -1,8 +1,13 @@
-##' @title Open file browser in working directory.
+##' @title
+##' Open file browser in working directory.
 ##'
-##' @description Open a file browser in the current working directory, on MS Windows only.
+##' @description
+##' Open a file browser in the current working directory, on MS Windows only.
 ##'
-##' @details openwd() opens a standard file browser in the current working directory. Currently it works on on MS Windows only. Does nothing but issues a warning on other platforms.
+##' @details
+##' \code{openwd()} opens a standard file browser in the current working directory.
+##' Currently it works on on MS Windows only. Does nothing but issues a
+##' warning on other platforms.
 ##'
 ##' @return
 ##' @author Dave Braze \email{davebraze@@gmail.com}
