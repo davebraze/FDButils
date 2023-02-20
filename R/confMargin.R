7a3c5bb9cd5ef6dbe4dcd522eb1e1772f71bff46
Dave Braze <davebraze@gmail.com>
2023-02-20 14:52:18 -0500

    add place holders for a couple of new convenience functions

---
+++ R/confMargin.R
@@ -0,0 +1,15 @@
+##' @title Confidence Margin (1/2 confidence interval)
+##'
+##' @description Compute the confidence margin of the mean for given
+##'
+##' @details
+##' Just a place holder for now.
+##'
+##' See, for example: https://www.statology.org/confidence-interval-in-r/
+##'
+##' blah, blah, blah
+##'
+##' @author David Braze \email{davebraze@@gmail.com}
+##' @aliases
+##'
+
---
+++ R/nin.R
@@ -0,0 +1,14 @@
+##' @title %nin%, not %in%
+##'
+##' @description Complement of base R %in% operator. Cribbed from {Hmisc}.
+##'
+##' @details
+##' Just a place holder for now. TODO: Copy the function over from {Hmisc}, or
+##' import Hmisc and re-export this operator.
+##'
+##' blah, blah, blah
+##'
+##' @author David Braze \email{davebraze@@gmail.com}
+##' @aliases
+##'
+
